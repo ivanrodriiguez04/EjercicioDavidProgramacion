26a4e82f106134dd15979413750f0cd9b727c58a
csi22-irodhan <ivanrodriiguez04@gmail.com>
2024-01-31 11:45:27 +0100

    31/01/2024

--- Servicios/OperacionalesImplementacion.cs
+++ Servicios/OperacionalesImplementacion.cs
@@ -35,17 +35,37 @@ namespace EjercicioDavidProgramacion.Servicios
             Console.WriteLine("Introduzca los apellidos del cliente: ");
             cliente.ApellidosCliente = Console.ReadLine();
             Console.WriteLine("Introduzca el telefono del cliente: ");
-            cliente.
+            cliente.TelefonoCliente = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el numero de la seguridad social: ");
+            cliente.NumeroSS=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el color del coche: ");
+            cliente.ColorCoche = Console.ReadLine();
+            Console.WriteLine("Introduzca el modelo del coche: ");
+            cliente.ModeloCoche = Console.ReadLine();
             return cliente;
         }
         public void modificarCliente(List<ClienteDto> listaClientesAntigua)
         {
-            throw new NotImplementedException();
+            string dni = pedirDNI();
         }
+
 
         public void validarCliente(List<ClienteDto> listaClientesAntigua)
         {
             throw new NotImplementedException();
+        } 
+        
+        /// <summary>
+        /// Metodo que pide el dni del cliente a validar o modificar
+        /// irodhan -> 31/01/2024
+        /// </summary>
+        /// <returns></returns>
+        private string pedirDNI() 
+        {
+            string DNI = "aaaaa";
+            Console.WriteLine("Introduzca el DNI del cliente a modificar: ");
+            DNI=Console.ReadLine();
+            return DNI;
         }
     }
 }
